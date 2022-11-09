e2cdd44fe5fabc19631a45b692dc428daa130911
aluno <aluno@ifpe>
2022-11-09 10:45:55 -0300

    Parte Vinicius

--- my-project/screens/Login.js
+++ my-project/screens/Login.js
@@ -14,13 +14,27 @@ export default function Login({route,navigation}){
   flexDirection: "column"
   }]}>
 
+<View style={{ flex: 0.3}} >
+
+<Icon
+  name='arrow-left'
+  color='black' 
+  size={20}/>
+
+</View>
+
+
 <View style={{ flex: 1,alignItems:"center", paddingTop:50}} >
+
+
+
+
   <Avatar
   rounded
   size="xlarge"
   source={{
     uri:
-      'https://static.vecteezy.com/ti/vetor-gratis/p1/5026528-vector-illustration-female-avatar-in-flat-style-gr%C3%A1tis-vetor.jpg',
+      '../../my-project/assets/Logo.png',
   }}
 />
 </View>
@@ -28,9 +42,20 @@ export default function Login({route,navigation}){
 
 
 <View style={{ flex: 1, alignItems:"center", paddingTop:20}}>
-<Input  leftIcon={{ type: 'font-awesome', name: 'user', color:'white' }} placeholder={' login'}/>
+<Input  leftIcon={{ type: 'font-awesome', name: 'user', color:'white' }} style={{width:"300px"}} placeholder={' login'}/>
 <Input  leftIcon={{ type: 'font-awesome', name: 'lock' , color:'white'}} placeholder={" senha"} secureTextEntry={true} />
 </View>
+
+<View style={{ flex: 2, alignItems:"center", paddingTop:20}}>
+<Button style={{ marginTop:50, width:150}} buttonStyle={{backgroundColor:"black", borderRadius: 20}} title="Entrar" onPress={()=>navigation.navigate('Login')}/>
+
+<Button style={{ marginTop:20, width:150}} title="Cadastre-se" buttonStyle={{backgroundColor:"black",  borderRadius: 20}} onPress={()=>navigation.navigate('')}/>
+
+<Button style={{ marginTop:20, width:200}} title="Esqueceu a Senha?"  titleStyle={{color: "black"}} buttonStyle={{backgroundColor:"white"}} onPress={()=>navigation.navigate('')}/>
+
+</View>
+
+
   
 </View>
 );};
@@ -39,6 +64,6 @@ const styles = StyleSheet.create({
   container: {
   flex: 1,
   padding: 20,
-  backgroundColor:'black'
+  backgroundColor:'white'
   },
   });
