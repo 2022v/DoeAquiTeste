3b23b9481f90a19309075f2a05525f07e3b1acd8
aluno <aluno@ifpe>
2022-11-09 10:57:42 -0300

    Parte de Bia

--- my-project/screens/Tela1.js
+++ my-project/screens/Tela1.js
@@ -4,27 +4,41 @@ import { Avatar } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
 import { Button } from 'react-native-elements';
-import cadastro from "./Cadastro";
-
+import { ActivityIndicator } from 'react-native';
+import { Image } from 'react-native-elements';
+ 
+ 
+ 
+ 
 export default function Tela1({route,navigation}){
-
-
-  return (
-
-  <View style={[styles.container, {
-  flexDirection: "column"
-  }]}>
-  
-<View >
-<Button onPress={()=>navigation.navigate('cadastro')}style={{ marginTop:50}} title="Entrar"/>
+ 
+ 
+ return (
+ 
+ <View style={[styles.container, {
+ flexDirection: "column"
+ }]}>
+ 
+<View>
+<Image
+ source={require('../../my-project/assets/Logo.png')}
+ style={{ marginTop:250, width: 400, height: 200 }}
+/>
+ 
+<Text style={{ marginTop:20, marginLeft:130, color:'white', fontSize:30}} h1> DOE AQUI! </Text>
+ 
+</View> 
+ 
+<View style={{ marginTop:200}} >
+<Button style={{ marginTop:100}} buttonStyle={{backgroundColor:'white', borderRadius:30, width:200, height:60, marginLeft:100}} titleStyle={{color:'black', fontSize:20}} type="clear" title="Doe Agora!"/>
 </View>
 </View>
 );};
-
+ 
 const styles = StyleSheet.create({
-  container: {
-  flex: 1,
-  padding: 20,
-  backgroundColor:'black'
-  },
+ container: {
+ flex: 1,
+ padding: 20,
+ backgroundColor:'black'
+ },
   });
