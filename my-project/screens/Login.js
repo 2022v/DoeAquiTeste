69ca0c515256b4e76c4d111c2ca05581598b0a75
Maria-Beatriz18 <mmbbeatriz.15@gmail.com>
2022-11-30 18:19:15 -0300

    Login

--- my-project/screens/Login.js
+++ my-project/screens/Login.js
@@ -4,9 +4,15 @@ import { Avatar } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
 import { Button } from 'react-native-elements';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from '../config/firebase';
+import { showMessage, hideMessage } from "react-native-flash-message";
 
 export default function Login({route,navigation}){
 
+
+  const [login, setLogin] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
   return (
 
@@ -31,14 +37,39 @@ export default function Login({route,navigation}){
 </View>
 
 
-
-<View style={{ flex: 1, alignItems:"center"}}>
-<Input  leftIcon={<Icon name='envelope'size={24} color='black'/>} style={{width:"300px"}} placeholder={' Email'}/>
-<Input  leftIcon={<Icon name='lock'size={24} color='black'/>} placeholder={" Senha"} secureTextEntry={true} />
+<View style={{ flex: 1, alignItems:"center", paddingTop:20}}>
+<Input  leftIcon={{ type: 'font-awesome', name: 'user', color:'black' }}
+ placeholder={' login'}
+ value={login}
+                onChangeText={login => setLogin(login)}
+                textContentType={'emailAddress'}
+                maxLength={255}/>
+<Input  leftIcon={{ type: 'font-awesome', name: 'lock' , color:'black'}}
+ placeholder={" senha"} 
+ value={password}
+                onChangeText={password => setPassword(password)}
+                secureTextEntry={true}
+                maxLength={255} />
 </View>
 
 <View style={{ flex: 1, alignItems:"center", paddingTop:20}}>
-<Button style={{ marginTop:50, width:150}} buttonStyle={{backgroundColor:"black", borderRadius: 20}} title="Entrar" onPress={()=>navigation.navigate('Home')}/>
+<Button title="Entrar" style={{ marginTop:50, width:150}} 
+buttonStyle={{backgroundColor:"black", borderRadius: 20}}
+onPress={() => {
+  signInWithEmailAndPassword(auth, login, password)
+      .then((userCredential) => {
+          navigation.navigate('Home');
+          setLogin('');
+          setPassword('');
+      })
+      .catch((error) => {
+          showMessage({
+              message: 'E-mail ou senha incorretos',
+              type: 'danger',
+          });
+      })
+}}
+/>
 
 <Button style={{ marginTop:20, width:150}} title="Cadastre-se" buttonStyle={{backgroundColor:"black",  borderRadius: 20}} onPress={()=>navigation.navigate('Cadastro')}/>
 
